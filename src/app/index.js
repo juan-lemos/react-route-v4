5e5f00aca716aae1729d0437b9911d601df10de7
juanpale <juanpale@users.noreply.github.com>
2017-08-08 16:22:27 -0300

    finish 11

--- src/app/index.js
+++ src/app/index.js
@@ -4,17 +4,21 @@ import { render } from "react-dom";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home"
 class App extends React.Component {
+    onGreet() {
+        alert("Hello!");
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink="Home"/>
+                        <Header homeLink="Home" />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} initialAge={27}/>
+                        <Home name={"Max"} initialAge={27} greet={this.onGreet}/>
                     </div>
                 </div>
             </div>
