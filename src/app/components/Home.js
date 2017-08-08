68fffb7cd1b27444c514e28d47554a937baa0e20
juanpale <juanpale@users.noreply.github.com>
2017-08-08 15:44:56 -0300

    finish part 9

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,17 +2,22 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
-    constructor(props){
+    constructor(props) {
         super();
         this.state = {
-            age:props.initialAge,
-            status:0
+            age: props.initialAge,
+            status: 0
         }
+        setTimeout(() => {
+            this.setState({
+                status : this.state.status + 1
+            })
+        }, 100)
     }
 
-    onMakeOlder(){
+    onMakeOlder() {
         this.setState({
-            age:this.state.age +3
+            age: this.state.age + 3
         });
     }
 
@@ -20,10 +25,10 @@ export class Home extends React.Component {
         return (
             <div>
                 <p>In a new Component!</p>
-                 <p>Your name is {this.props.name}, your initialAge is {this.state.age}</p>
-                 <p>Status: {this.state.status}</p>  
-                <hr/>
-                 <button onClick={()=> this.onMakeOlder()} className="btn btn-primary">Make me older</button> 
+                <p>Your name is {this.props.name}, your initialAge is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
+                <hr />
+                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older</button>
             </div>
         )
     }
